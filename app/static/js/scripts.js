7f670b2d7da2d9183f308966dfeff254313839ea
Mark van Heeswijk <mark@mediahost.co.za>
2023-05-19 15:34:43 +0200

    js script to highlight nav items

---
+++ app/static/js/scripts.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Highlight the section headings as you scroll through the page
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+});
