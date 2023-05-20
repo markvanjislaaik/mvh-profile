504d07539c5fb97b01c81f8b8c06f1892a14ab17
Mark van Heeswijk <mark@mediahost.co.za>
2023-05-20 06:51:15 +0200

    downladable cv route and pdf file

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, send_from_directory
 from app import app
 
 from internal import profile
@@ -9,3 +9,8 @@ def index():
     context = profile.get_profile()
 
     return render_template('index.html', **context)
+
+
+@app.route('/download_pdf')
+def download_pdf():
+    return send_from_directory('static/', 'pdf/Mark van Heeswijk CV - Python Developer 2023.pdf', as_attachment=True)
